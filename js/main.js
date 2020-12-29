4ae1b91989aff79c3af92a28dd00cbdbf783f4d6
Marcos Ribeiro <slproger@gmail.com>
2020-12-29 02:59:00 -0300

    criando a cobrinha

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+};
 
 function criarBG() {
   context.fillStyle = "lightGreen";
   context.fillRect(0,0,16 * box, 16 * box);
 }
 
+function criaCobrinha() {
+  snake.forEach(e => {
+    context.fillStyle = "green";
+    context.fillRect(e.x, e.y, box, box);    
+  });
+}
+
 criarBG();
+criaCobrinha();
